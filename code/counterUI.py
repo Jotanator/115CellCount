383653aa4123f3f8d31d6ccd9dcbb19ca84fd3ce
Sriram Ramesh <sramesh5@ucsc.edu>
2021-05-17 10:03:24 -0700

    Change variable names

--- code/counterUI.py
+++ code/counterUI.py
@@ -8,37 +8,37 @@ def initUI():
     root = tk.Tk()
     root.title("Cell Counting Application")
 
-    canvas1=tk.Canvas(root, width=1000, height=1000, relief="raised")
-    canvas1.pack()
+    canvas=tk.Canvas(root, width=1000, height=1000, relief="raised")
+    canvas.pack()
     
-    return root, canvas1
+    return root, canvas
     
 def showUI(root):
     root.mainloop()
 
 
-def appHeader(root, canvas1, outputHandler):
+def appHeader(root, canvas, outputHandler):
     label1=tk.Label(root, text="Cell Counting Application!")
     label1.config(font=("helvetica", 14))
-    canvas1.create_window(500, 25, window=label1)
+    canvas.create_window(500, 25, window=label1)
 
     
-def showFileUploadButton(root, canvas1, outputHandler):
+def showFileUploadButton(root, canvas, outputHandler):
     label2=tk.Label(root, text="Browse for image:")
     label2.config(font=("helvetica", 10))
 
-    canvas1.create_window(500, 50, window=label2)
+    canvas.create_window(500, 50, window=label2)
 
-    browseButton=tk.Button(text="Browse", command= lambda: (browseFilesys(root, canvas1, outputHandler)), bg="brown", fg="white", font=("helvetica", 9, "bold"))
-    canvas1.create_window(500, 100, window=browseButton)
+    browseButton=tk.Button(text="Browse", command= lambda: (browseFilesys(root, canvas, outputHandler)), bg="brown", fg="white", font=("helvetica", 9, "bold"))
+    canvas.create_window(500, 100, window=browseButton)
     
-def browseFilesys(root, canvas1, outputHandler):
+def browseFilesys(root, canvas, outputHandler):
     filename = filedialog.askopenfilename()
     try:
         background_image=ImageTk.PhotoImage(Image.open(filename))
     except:
         label3=tk.Label(root, text="Please input a valid relative or absolute file location", font=("helvetica", 10))
-        canvas1.create_window(500, 250, window=label3)
+        canvas.create_window(500, 250, window=label3)
         return
     background_label=tk.Label(root, image=background_image)
     background_label.place(x=0, y=150, relwidth=1, relheight=1)
@@ -46,18 +46,18 @@ def browseFilesys(root, canvas1, outputHandler):
     root.grid()
 
     label3=tk.Label(root, text="Image is: "+filename, font=("helvetica", 10))
-    canvas1.create_window(500, 125, window=label3)
+    canvas.create_window(500, 125, window=label3)
 
     countMsg = "There are "+ str(outputHandler.getPrediction()) + " cells in this image"
     label4=tk.Label(root, text=countMsg, font=("helvetica", 10))
-    canvas1.create_window(500, 150, window=label4)
+    canvas.create_window(500, 150, window=label4)
     
-    showCSVDownloadButton(canvas1, filename, 17, 0.4)
+    showCSVDownloadButton(canvas, filename, 17, 0.4)
     
 
-def showCSVDownloadButton(canvas1, filename, number, percent):
+def showCSVDownloadButton(canvas, filename, number, percent):
     downloadButton=tk.Button(text="Download information about file", command= lambda: (writeToCSV(filename, number, percent)), bg="brown", fg="white", font=("helvetica", 9, "bold"))
-    canvas1.create_window(500, 200, window=downloadButton)
+    canvas.create_window(500, 200, window=downloadButton)
     
 def writeToCSV(filename, number, percent):
     with open(filename+'.csv', 'w', newline='') as file:
