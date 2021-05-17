32415bf6702b3e57b3a89e5f2134976b72fb77f3
Sriram Ramesh <sramesh5@ucsc.edu>
2021-05-17 07:22:18 -0700

    Initial segmentation of counterUI.py code to multiple functions

--- code/counterUI.py
+++ code/counterUI.py
@@ -4,6 +4,9 @@ from tkinter import filedialog
 import csv
 import modelOutputInterface as mOI
 
+
+
+
 def showUI(outputHandler):
     root= tk.Tk()
 
@@ -19,43 +22,41 @@ def showUI(outputHandler):
     label2=tk.Label(root, text="Browse for image:")
     label2.config(font=("helvetica", 10))
 
-    def browseFilesys():
-        filename = filedialog.askopenfilename()
-        try:
-            background_image=ImageTk.PhotoImage(Image.open(filename))
-        except:
-            label3=tk.Label(root, text="Please input a valid relative or absolute file location", font=("helvetica", 10))
-            canvas1.create_window(500, 250, window=label3)
-            return
-        background_label=tk.Label(root, image=background_image)
-        background_label.place(x=0, y=150, relwidth=1, relheight=1)
-        root.photo = background_image
-        root.grid()
-
-        label3=tk.Label(root, text="Image is: "+filename, font=("helvetica", 10))
-        canvas1.create_window(500, 125, window=label3)
-
-        countMsg = "There are "+ str(outputHandler.getPrediction()) + " cells in this image"
-        label4=tk.Label(root, text=countMsg, font=("helvetica", 10))
-        canvas1.create_window(500, 150, window=label4)
-
-        number = 14;
-        percent = .40
-
-        def writeToCSV():
-                with open(filename+'.csv', 'w', newline='') as file:
-                    writer = csv.writer(file)
-                writer.writerow(["This is the information for "+str(filename)])
-                writer.writerow(["Number of cells is: "+str(number)])
-                writer.writerow(["Percent of alive cells is: "+str(percent*100)+"%"])
-                writer.writerow(["This is all hardcoded"])
-
-        downloadButton=tk.Button(text="Download information about file", command=writeToCSV, bg="brown", fg="white", font=("helvetica", 9, "bold"))
-        canvas1.create_window(500, 200, window=downloadButton)
-
     canvas1.create_window(500, 50, window=label2)
     
-    browseButton=tk.Button(text="Browse", command=browseFilesys, bg="brown", fg="white", font=("helvetica", 9, "bold"))
+    browseButton=tk.Button(text="Browse", command=browseFilesys(root, canvas1, outputHandler), bg="brown", fg="white", font=("helvetica", 9, "bold"))
     canvas1.create_window(500, 100, window=browseButton)
 
     root.mainloop()
+    
+def browseFilesys(root, canvas1, outputHandler):
+    filename = filedialog.askopenfilename()
+    try:
+        background_image=ImageTk.PhotoImage(Image.open(filename))
+    except:
+        label3=tk.Label(root, text="Please input a valid relative or absolute file location", font=("helvetica", 10))
+        canvas1.create_window(500, 250, window=label3)
+        return
+    background_label=tk.Label(root, image=background_image)
+    background_label.place(x=0, y=150, relwidth=1, relheight=1)
+    root.photo = background_image
+    root.grid()
+
+    label3=tk.Label(root, text="Image is: "+filename, font=("helvetica", 10))
+    canvas1.create_window(500, 125, window=label3)
+
+    countMsg = "There are "+ str(outputHandler.getPrediction()) + " cells in this image"
+    label4=tk.Label(root, text=countMsg, font=("helvetica", 10))
+    canvas1.create_window(500, 150, window=label4)
+    
+    downloadButton=tk.Button(text="Download information about file", command=writeToCSV(filename, 14, 0.4), bg="brown", fg="white", font=("helvetica", 9, "bold"))
+    canvas1.create_window(500, 200, window=downloadButton)
+    
+    
+def writeToCSV(filename, number, percent):
+    with open(filename+'.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+    writer.writerow(["This is the information for "+str(filename)])
+    writer.writerow(["Number of cells is: "+str(number)])
+    writer.writerow(["Percent of alive cells is: "+str(percent*100)+"%"])
+    writer.writerow(["This is all hardcoded"])
