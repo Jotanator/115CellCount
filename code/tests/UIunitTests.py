054a7e3ce5f04c25a63aa764e6573833b39edace
Sriram Ramesh <sramesh5@ucsc.edu>
2021-05-17 10:18:15 -0700

    Add new unit tests to UIunitTests.py

--- code/tests/UIunitTests.py
+++ code/tests/UIunitTests.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 import os
 import sys
 import inspect
@@ -11,13 +13,32 @@ import modelOutputInterface as mOI
 import tests.TestModelOutputHandler as TMO
 import counterUI
 
+def minimalUITest():
+    root, canvas = counterUI.initUI()
+    counterUI.showUI(root)
+    
+def customElementUITest():
+    root, canvas = counterUI.initUI()
+    counterUI.appHeader(root, canvas)
+    
+    label=tk.Label(root, text="Example Label")
+    label.config(font=("helvetica", 14))
+    canvas.create_window(500, 55, window=label)
+    
+    fakeButton=tk.Button(text="Dummy Button", command= None, bg="brown", fg="white", font=("helvetica", 9, "bold"))
+    canvas.create_window(500, 100, window=fakeButton)
+    
+    counterUI.showUI(root)
+
 def modelOutputDummyTest():
     outputHandler = TMO.TestModelOutputHandler()
     root, canvas = counterUI.initUI()
-    counterUI.appHeader(root, canvas, outputHandler)  #any exceptions is a failure
+    counterUI.appHeader(root, canvas)  #any exceptions is a failure
     counterUI.showFileUploadButton(root, canvas, outputHandler)
     counterUI.showUI(root)
     
 
 if __name__ == '__main__':
+    minimalUITest()
+    customElementUITest()
     modelOutputDummyTest()
