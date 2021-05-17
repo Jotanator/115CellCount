08d654a8225f71f331f69d8cbd4fc90a5a74aa2a
Sriram Ramesh <sramesh5@ucsc.edu>
2021-05-16 18:47:56 -0700

    Convert the interface into an informal interface

--- code/modelOutputInterface.py
+++ code/modelOutputInterface.py
@@ -1,12 +1,5 @@
-class ModelOutputMeta(type):
+class ModelOutputHandlerInterface():
     """Any handler that connects the Model's predictions to the UI must implement the following methods"""
 
-    def __instancecheck__(cls, instance):
-        return cls.__subclasscheck__(type(instance))
-
-    def __subclasscheck__(cls, subclass):
-        return (hasattr(subclass, 'getPrediction') and 
-                callable(subclass.getPrediction) and 
-
-class ModelOutputInterface(metaclass=ModelOutputMeta):
-    pass
+    def getPrediction() -> int:
+        pass
