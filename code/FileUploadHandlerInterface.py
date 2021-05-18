d936f4143244d0ed9456a3d23761b2384107b1be
Sriram Ramesh <sramesh5@ucsc.edu>
2021-05-18 15:32:12 -0700

    Create an interface for File upload hanlders

---
+++ code/FileUploadHandlerInterface.py
@@ -0,0 +1,5 @@
+class FileUploadHandlerInterface():
+    """Any handler that connects the Model's predictions to the UI must implement the following methods"""
+
+    def openUploadPrompt():
+        pass
