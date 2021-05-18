1214b3095b78218c1249ffcc81cc455fb4b6b8f9
Sriram Ramesh <sramesh5@ucsc.edu>
2021-05-18 15:42:26 -0700

    Change abstract function signature

--- code/FileUploadHandlerInterface.py
+++ code/FileUploadHandlerInterface.py
@@ -1,5 +1,6 @@
+import Image
 class FileUploadHandlerInterface():
     """Any handler that allows the user to select a file from their computer for use in the program."""
 
-    def openUploadPrompt():
+    def browseFilesys()->Image:
         pass
