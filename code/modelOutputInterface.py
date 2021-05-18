3029ec348112700fe9050538e33a9c4b1a3cd976
DemPeeps <dhruvtummala@gmail.com>
2021-05-18 13:15:11 -0700

    Removed contents of branch

--- code/modelOutputInterface.py
+++
@@ -1,5 +0,0 @@
-class ModelOutputHandlerInterface():
-    """Any handler that connects the Model's predictions to the UI must implement the following methods"""
-
-    def getPrediction() -> int:
-        pass
--- code/tests/TestModelOutputHandler.py
+++
@@ -1,6 +0,0 @@
-import modelOutputInterface as mOI
-
-class TestModelOutputHandler(mOI.ModelOutputHandlerInterface):
-    """Extract text from an email."""
-    def getPrediction(self):
-       return 5 
