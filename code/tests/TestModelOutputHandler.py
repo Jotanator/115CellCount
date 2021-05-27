6645c414ebf2c5456f69070b064f5ab4a50754e0
Siram-Ramesh <sramesh5@ucsc.edu>
2021-05-27 13:13:50 -0700

    Fix lint errors in test code

--- code/tests/TestModelOutputHandler.py
+++ code/tests/TestModelOutputHandler.py
@@ -1,6 +1,8 @@
 import modelOutputInterface as mOI
 
+
 class TestModelOutputHandler(mOI.ModelOutputHandlerInterface):
     """Extract text from an email."""
+
     def getPrediction(self):
-       return 5 
+        return 5
--- code/tests/UIunitTests.py
+++ code/tests/UIunitTests.py
@@ -1,19 +1,22 @@
+import counterUI
+import TestModelOutputHandler as TMO
+import modelOutputInterface as mOI
 import os
 import sys
 import inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(
+    os.path.abspath(
+        inspect.getfile(
+            inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 
-import modelOutputInterface as mOI
-import TestModelOutputHandler as TMO
-import counterUI
-
 def modelOutputDummyTest():
     outputHandler = TMO.TestModelOutputHandler()
-    counterUI.showUI(outputHandler)  #any exceptions is a failure
+    counterUI.showUI(outputHandler)  # any exceptions is a failure
+
 
 if __name__ == '__main__':
     modelOutputDummyTest()
